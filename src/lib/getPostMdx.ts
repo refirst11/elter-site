94cae203eae02b30bc43eed395c0a379e4db9680
S.Watanabe <ocelot2828@gmail.com>
2024-06-13 19:31:28 +0900

    feat(getPostMdx): Added css-variables

--- src/lib/getPostMdx.ts
+++ src/lib/getPostMdx.ts
@@ -6,6 +6,7 @@ import { Tabs, Tab } from 'components/Tabs'
 import { TocBot } from 'components/TocBot'
 import rehypePrettyCode from 'rehype-pretty-code'
 import rehypeSlug from 'rehype-slug'
+import { createCssVariablesTheme } from 'shiki'
 
 const Element = {
   Tabs,
@@ -13,10 +14,18 @@ const Element = {
   TocBot
 }
 
+const myTheme = createCssVariablesTheme({
+  name: 'css-variables',
+  variablePrefix: '--shiki-',
+  variableDefaults: {},
+  fontStyle: true
+})
+
 const getPostMdx = async (slug: string): Promise<ReturnData> => {
   const folder = path.join(process.cwd(), '/src/documentation')
   const fullPath = path.join(folder, `${slug}.mdx`)
   const file = fs.readFileSync(fullPath, 'utf8')
+
   const { frontmatter, content } = await compileMDX<PostData>({
     components: Element,
     source: file,
@@ -28,8 +37,8 @@ const getPostMdx = async (slug: string): Promise<ReturnData> => {
           [
             rehypePrettyCode,
             {
-              theme: 'github-light',
-              keepBackground: true,
+              theme: myTheme,
+              keepBackground: false,
               onVisitHighlightedLine(node: { properties: { className: string[] } }) {
                 node.properties.className.push('highlighted')
               }

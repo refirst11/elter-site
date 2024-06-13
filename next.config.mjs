7dfe8b6608636ce71ac85c6c41f36b801a80569d
S.Watanabe <ocelot2828@gmail.com>
2024-06-13 19:28:06 +0900

    refactor: remove syntax plugins

--- next.config.mjs
+++ next.config.mjs
@@ -1,25 +1,10 @@
 import withMDX from '@next/mdx'
-import rehypePrettyCode from 'rehype-pretty-code'
-import { getHighlighter } from 'shiki'
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   pageExtensions: ['js', 'jsx', 'mdx', 'ts', 'tsx']
 }
 
-const withMDXConfig = withMDX({
-  options: {
-    rehypePlugins: [
-      [
-        rehypePrettyCode,
-        {
-          theme: 'github-light',
-          keepBacground: true,
-          getHighlighter
-        }
-      ]
-    ]
-  }
-})
+const withMDXConfig = withMDX()
 
 export default withMDXConfig(nextConfig)

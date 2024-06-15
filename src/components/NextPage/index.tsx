0c09589af08988f486e720d9fee70fedbb50467b
S.Watanabe <ocelot2828@gmail.com>
2024-06-15 22:18:05 +0900

    feat(NextPage): Added contents

--- src/components/NextPage/index.tsx
+++ src/components/NextPage/index.tsx
@@ -1,16 +1,49 @@
 'use client'
 
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { styles } from './style.css'
 import Link from 'next/link'
 import { usePathname } from 'next/navigation'
+import getAllPosts from 'lib/getAllPosts'
+import PostsData from 'types/PostsData'
 
 export const NextPage = () => {
   const pathname = usePathname()
-  const links = [
-    { href: '/', label: 'Home' },
-    { href: '/docs', label: 'Documentation' }
-  ]
+  const [prevPost, setPrevPost] = useState<PostsData | null>(null)
+  const [nextPost, setNextPost] = useState<PostsData | null>(null)
 
-  return <div className={styles.container}>{'<'}</div>
+  useEffect(() => {
+    const fetchPosts = async () => {
+      const posts = await getAllPosts()
+      const currentIndex = posts.findIndex((post) => `/${post.slug}` === pathname)
+
+      if (currentIndex > 0) {
+        setPrevPost(posts[currentIndex - 1])
+      } else {
+        setPrevPost(null)
+      }
+      if (currentIndex < posts.length - 1) {
+        setNextPost(posts[currentIndex + 1])
+      } else {
+        setNextPost(null)
+      }
+    }
+
+    fetchPosts()
+  }, [pathname])
+
+  return (
+    <div className={styles.container}>
+      {prevPost && (
+        <Link href={`/${prevPost.slug}`} className={styles.prev}>
+          {`< ${prevPost.title}`}
+        </Link>
+      )}
+      {nextPost && (
+        <Link href={`/${nextPost.slug}`} className={styles.next}>
+          {`${nextPost.title} >`}
+        </Link>
+      )}
+    </div>
+  )
 }
--- src/components/NextPage/style.css.ts
+++ src/components/NextPage/style.css.ts
@@ -1,12 +1,46 @@
-import { Scoped, media } from 'typedcssx'
+import { Style, media } from 'typedcssx'
 
-export const styles = Scoped.sheet({
+export const styles = Style.create({
   container: {
+    zIndex: '1',
     position: 'relative',
+    top: '100px',
     height: '92px',
     width: '100%',
     padding: '32px 0',
     marginTop: '16px',
-    borderTop: 'solid 1px var(--color-border)'
+    display: 'flex',
+    justifyContent: 'space-between'
+  },
+  prev: {
+    position: 'absolute',
+    textDecoration: 'none',
+    left: 0,
+    width: 200,
+    height: 60,
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
+    padding: '24px 48px',
+    borderRadius: '12px',
+    background: 'var(--color-card)',
+    hover: {
+      border: 'solid 0.4px var(--color-link)'
+    }
+  },
+  next: {
+    position: 'absolute',
+    textDecoration: 'none',
+    right: 0,
+    width: 200,
+    height: 60,
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
+    borderRadius: '12px',
+    background: 'var(--color-card)',
+    hover: {
+      border: 'solid 0.4px var(--color-link)'
+    }
   }
 })

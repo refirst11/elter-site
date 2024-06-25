38e2240530c287f751dda66895b5942a55ad5682
S.Watanabe <ocelot2828@gmail.com>
2024-06-25 17:09:26 +0900

    feat(handleLinkCick.ts): cut to lib

---
+++ src/lib/handleLinkClick.ts
@@ -0,0 +1,18 @@
+import { AppRouterInstance } from 'next/dist/shared/lib/app-router-context.shared-runtime'
+
+export const handleLinkClick = (router: AppRouterInstance, e: React.MouseEvent, slug: string, headingId: string) => {
+  e.preventDefault()
+  e.stopPropagation()
+
+  setTimeout(() => {
+    const element = document.getElementById(headingId)
+    if (element) {
+      const offsetTop = element.getBoundingClientRect().top + window.scrollY - 100
+      window.scrollTo({
+        top: offsetTop
+      })
+    }
+  }, 200)
+
+  router.push(`/${slug}#${headingId}`)
+}

8c344aa634fd74de841bb4d2b0dcd59192db9cf8
S.Watanabe <ocelot2828@gmail.com>
2024-06-27 18:27:51 +0900

    feat(typedcss_ogp.png): add ogp card

--- src/lib/generateSEOData.ts
+++ src/lib/generateSEOData.ts
@@ -1,6 +1,7 @@
 import SeoData from 'types/SeoData'
 
 const generateSEOData = ({ title, subtitle, date }: SeoData) => {
+  const url = process.env.PROD_URL
   return {
     title: title,
     description: subtitle,
@@ -17,7 +18,7 @@ const generateSEOData = ({ title, subtitle, date }: SeoData) => {
       url: process.env.PROD_URL,
       type: 'website',
       images: {
-        url: `${process.env.PROD_URL}/api/ogp?title=${title}&date=${date}`,
+        url: url + '/typedcssx_ogp.png',
         width: 1200,
         height: 600
       }
@@ -27,7 +28,7 @@ const generateSEOData = ({ title, subtitle, date }: SeoData) => {
       title: title,
       description: subtitle,
       images: {
-        url: `${process.env.PROD_URL}/api/ogp?title=${title}&date=${date}`,
+        url: url + '/typedcssx_ogp.png',
         width: 1200,
         height: 600
       },

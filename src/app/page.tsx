4f59d77fc0f501997d2de2e9b15c36b0b71b4e28
S.Watanabe <ocelot2828@gmail.com>
2024-06-27 14:34:14 +0900

    feat(Home): Added generateSEOData

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,8 @@
 import { Home } from 'components/Home'
+import generateSEOData from 'lib/generateSEOData';
+import { Metadata } from 'next';
+
+export const metadata: Metadata = generateSEOData({ title: 'Home', subtitle: 'typedcssx homepage' });
 
 function Page() {
   return <Home />

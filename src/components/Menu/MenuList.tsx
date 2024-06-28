2d83cd4776e4840418141c31ebb8d4ec1a93f4e4
S.Watanabe <ocelot2828@gmail.com>
2024-06-28 16:55:42 +0900

    refactor(*): Moved headings to lib

--- src/components/Menu/MenuList.tsx
+++ src/components/Menu/MenuList.tsx
@@ -7,6 +7,7 @@ import type PostsData from 'types/PostsData'
 import { usePathname, useRouter } from 'next/navigation'
 import { IoMdArrowDropright } from 'react-icons/io'
 import { handleLinkClick } from 'lib/handleLinkClick'
+import { headings } from 'lib/headings'
 
 type MenuProps = {
   posts: PostsData[]
@@ -33,7 +34,7 @@ const MenuList = ({ posts }: MenuProps) => {
 
   useEffect(() => {
     const handleScroll = () => {
-      const headingElements = document.querySelectorAll('h1, h2, h3, h4, h5, h6')
+      const headingElements = document.querySelectorAll('h2, h3')
       let currentActiveHeading = ''
 
       headingElements.forEach((heading) => {
@@ -53,60 +54,6 @@ const MenuList = ({ posts }: MenuProps) => {
       window.removeEventListener('scroll', handleScroll)
     }
   }, [])
-
-  const headings = [
-    {
-      heading: [
-        { id: 'zero-runtime-and-rsc', text: '# Zero Runtime and RSC' },
-        { id: 'hard-type-and-hot-reload', text: '# Hard Type and Hot Reload' },
-        { id: 'production-environment', text: '# Production environment' },
-        { id: 'inspirations', text: '# Inspirations' },
-        { id: 'whats-next', text: "# What'sNext" }
-      ]
-    },
-    {
-      heading: [
-        { id: 'npm', text: '# NPM' },
-        { id: 'build-setting', text: '# Build setting' },
-        { id: 'tsconfigjson', text: '# tsconfig.json' },
-        { id: 'global-style-sheet', text: '# Global style sheet' },
-        { id: 'supported-nodejs-versions-v18---v22', text: '# Supported Node.js Versions: v18 - v22' }
-      ]
-    },
-    {
-      heading: [
-        { id: 'stylecreate', text: '# Style.create' },
-        { id: 'compiled-create', text: '# Compiled create' },
-        { id: 'styleglobal', text: '# Style.global' },
-        { id: 'compiled-global', text: '# Compiled global' },
-        { id: 'styleset', text: '# Style.set' },
-        { id: 'compiled-set', text: '# Compiled set' },
-        { id: 'styleroot', text: '# Style.root' },
-        { id: 'compiled-root', text: '# Compiled root' }
-      ]
-    },
-    {
-      heading: [
-        { id: 'input', text: '# Input' },
-        { id: 'output', text: '# Output' }
-      ]
-    },
-    {
-      heading: [
-        { id: 'types', text: '# Types' },
-        { id: 'pseudo-hover-example', text: '# Pseudo hover example' },
-        { id: 'compiled-hover', text: '# Compiled hover' },
-        { id: 'pseudo-lang-example', text: '# Pseudo lang example' },
-        { id: 'compiled-lang', text: '# Compiled lang' },
-        { id: 'pseudo-not-example', text: '# Pseudo not example' },
-        { id: 'compiled-not', text: '# Compiled not' },
-        { id: 'pseudo-has-example', text: '# Pseudo has example' },
-        { id: 'compiled-has', text: '# Compiled has' },
-        { id: 'and-string-selector', text: '# And string Selector' },
-        { id: 'compiled-and-string-selector', text: '# Compiled and string Selector' }
-      ]
-    }
-  ]
 
   useEffect(() => {
     if (pathname !== '/') setIsListVisible(true)
---
+++ src/lib/headings.ts
@@ -0,0 +1,53 @@
+export const headings = [
+  {
+    heading: [
+      { id: 'zero-runtime-and-rsc', text: '# Zero Runtime and RSC' },
+      { id: 'hard-type-and-hot-reload', text: '# Hard Type and Hot Reload' },
+      { id: 'production-environment', text: '# Production environment' },
+      { id: 'inspirations', text: '# Inspirations' },
+      { id: 'whats-next', text: "# What'sNext" }
+    ]
+  },
+  {
+    heading: [
+      { id: 'npm', text: '# NPM' },
+      { id: 'build-setting', text: '# Build setting' },
+      { id: 'tsconfigjson', text: '# tsconfig.json' },
+      { id: 'global-style-sheet', text: '# Global style sheet' },
+      { id: 'supported-nodejs-versions-v18---v22', text: '# Supported Node.js Versions: v18 - v22' }
+    ]
+  },
+  {
+    heading: [
+      { id: 'stylecreate', text: '# Style.create' },
+      { id: 'compiled-create', text: '# Compiled create' },
+      { id: 'styleglobal', text: '# Style.global' },
+      { id: 'compiled-global', text: '# Compiled global' },
+      { id: 'styleset', text: '# Style.set' },
+      { id: 'compiled-set', text: '# Compiled set' },
+      { id: 'styleroot', text: '# Style.root' },
+      { id: 'compiled-root', text: '# Compiled root' }
+    ]
+  },
+  {
+    heading: [
+      { id: 'input', text: '# Input' },
+      { id: 'output', text: '# Output' }
+    ]
+  },
+  {
+    heading: [
+      { id: 'types', text: '# Types' },
+      { id: 'pseudo-hover-example', text: '# Pseudo hover example' },
+      { id: 'compiled-hover', text: '# Compiled hover' },
+      { id: 'pseudo-lang-example', text: '# Pseudo lang example' },
+      { id: 'compiled-lang', text: '# Compiled lang' },
+      { id: 'pseudo-not-example', text: '# Pseudo not example' },
+      { id: 'compiled-not', text: '# Compiled not' },
+      { id: 'pseudo-has-example', text: '# Pseudo has example' },
+      { id: 'compiled-has', text: '# Compiled has' },
+      { id: 'and-string-selector', text: '# And string Selector' },
+      { id: 'compiled-and-string-selector', text: '# Compiled and string Selector' }
+    ]
+  }
+]

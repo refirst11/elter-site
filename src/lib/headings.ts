f28ac1542653d52dbcb033c725c9bd6bcb15df61
S.Watanabe <ocelot2828@gmail.com>
2024-08-17 17:53:11 +0900

    fix(headings): Some parts were deleted due to the update.

--- src/lib/headings.ts
+++ src/lib/headings.ts
@@ -11,50 +11,10 @@ export const headings = [
   {
     heading: [
       { id: 'npm', text: '# NPM' },
-      { id: 'build-setting', text: '# Build setting' },
+      { id: 'compiler-setting', text: '# Compiler setting' },
       { id: 'tsconfigjson', text: '# tsconfig.json' },
       { id: 'global-style-sheet', text: '# Global style sheet' },
-      { id: 'support-nodejs-v18---22', text: '# Support Node.js v18 - v22' }
-    ]
-  },
-  {
-    heading: [
-      { id: 'stylecreate', text: '# Style.create' },
-      { id: 'compiled-create', text: '# Compiled create' },
-      { id: 'styleglobal', text: '# Style.global' },
-      { id: 'compiled-global', text: '# Compiled global' },
-      { id: 'styleset', text: '# Style.set' },
-      { id: 'compiled-set', text: '# Compiled set' },
-      { id: 'styleroot', text: '# Style.root' },
-      { id: 'compiled-root', text: '# Compiled root' }
-    ]
-  },
-  {
-    heading: [
-      { id: 'wrapper-selector', text: '# Wrapper selectors' },
-      { id: 'pseudo-hover-example', text: '# Pseudo hover example' },
-      { id: 'compiled-hover', text: '# Compiled hover' },
-      { id: 'pseudo-lang-example', text: '# Pseudo lang example' },
-      { id: 'compiled-lang', text: '# Compiled lang' },
-      { id: 'pseudo-not-example', text: '# Pseudo not example' },
-      { id: 'compiled-not', text: '# Compiled not' },
-      { id: 'pseudo-has-example', text: '# Pseudo has example' },
-      { id: 'compiled-has', text: '# Compiled has' },
-      { id: '-string-selector', text: '# & string Selector' },
-      { id: 'compiled--string-selector', text: '# Compiled & string Selector' }
-    ]
-  },
-  {
-    heading: [
-      { id: 'input', text: '# Input' },
-      { id: 'output', text: '# Output' }
-    ]
-  },
-  {
-    heading: [
-      { id: 'input', text: '# Input' },
-      { id: 'output', text: '# Output' },
-      { id: 'uses', text: '# Uses' }
+      { id: 'support-nodejs', text: '# Support Node.js' }
     ]
   }
 ]

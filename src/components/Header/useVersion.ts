83138a820556399af435a6b92c4fff30bb9156f8
S.Watanabe <ocelot2828@gmail.com>
2024-08-23 19:13:59 +0900

    feat(version): add  swr and api route

--- src/components/Header/useVersion.ts
+++ src/components/Header/useVersion.ts
@@ -1,15 +1,13 @@
 'use client'
 
 import useSWR from 'swr'
-import { NpmPackage } from 'types/NpmPackage'
 
-const fetcher = (url: string): Promise<NpmPackage> => fetch(url).then((res) => res.json())
+const fetcher = (url: string) => fetch(url).then((res) => res.json())
 
 export const useVersion = () => {
-  const url = process.env.VERSION || ''
-  const { data, error } = useSWR<NpmPackage>(url, fetcher)
+  const { data, error } = useSWR<{ version: string }>('/api/version', fetcher)
 
-  const version = data?.['dist-tags'].latest || null
+  const version = data?.version || null
 
   return {
     version,

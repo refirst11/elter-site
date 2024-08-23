32358423ff343900a67007892375f99049a65d39
S.Watanabe <ocelot2828@gmail.com>
2024-08-23 20:30:54 +0900

    fix(Header*): swr to normal fetch api

---
+++ src/components/Header/Headers.tsx
@@ -0,0 +1,37 @@
+'use client'
+
+import '../../styles/global.css'
+import React from 'react'
+import { styles } from './style.css'
+import Link from 'next/link'
+import isCurrentLink from 'lib/isCurrentLink'
+import { usePathname } from 'next/navigation'
+import { ToggleDarkMode } from 'components/ToggleDarkMode'
+import { GitIcon } from 'components/GitIcon'
+import { SearchBox } from 'components/SearchBox'
+
+export const Headers = ({ version }: { version: string }) => {
+  const pathname = usePathname()
+  const links = [
+    { href: '/', label: 'Home' },
+    { href: '/docs', label: 'Documentation' }
+  ]
+
+  return (
+    <header className={styles.container}>
+      <Link href="/" className={`${styles.logo} ${pathname == '/' ? styles.ontline : ''}`}>
+        typedcssx <span className={styles.version}>v{version}</span>
+      </Link>
+      <GitIcon />
+      <ToggleDarkMode />
+      <SearchBox />
+      <nav className={styles.wrapper}>
+        {links.map((link) => (
+          <Link key={link.href} href={link.href} className={styles.link + ' ' + styles.active} aria-current={isCurrentLink(link.href, pathname) ? 'page' : 'false'}>
+            {link.label}
+          </Link>
+        ))}
+      </nav>
+    </header>
+  )
+}
--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,39 +1,17 @@
-'use client'
+import { Headers } from './Headers'
+import { NpmPackage } from 'types/NpmPackage'
 
-import '../../styles/global.css'
-import React from 'react'
-import { styles } from './style.css'
-import Link from 'next/link'
-import isCurrentLink from 'lib/isCurrentLink'
-import { usePathname } from 'next/navigation'
-import { ToggleDarkMode } from 'components/ToggleDarkMode'
-import { GitIcon } from 'components/GitIcon'
-import { SearchBox } from 'components/SearchBox'
-import { useVersion } from './useVersion'
+const getVersion = async (): Promise<NpmPackage> => {
+  const url = process.env.VERSION_URL || ''
+  const response = await fetch(url, { next: { revalidate: 3600 * 24 * 7 } }) // 1week
 
-export const Header = () => {
-  const { version } = useVersion()
-  const pathname = usePathname()
-  const links = [
-    { href: '/', label: 'Home' },
-    { href: '/docs', label: 'Documentation' }
-  ]
+  const data: NpmPackage = await response.json()
+  return data
+}
 
-  return (
-    <header className={styles.container}>
-      <Link href="/" className={`${styles.logo} ${pathname == '/' ? styles.ontline : ''}`}>
-        typedcssx <span className={styles.version}>v{version}</span>
-      </Link>
-      <GitIcon />
-      <ToggleDarkMode />
-      <SearchBox />
-      <nav className={styles.wrapper}>
-        {links.map((link) => (
-          <Link key={link.href} href={link.href} className={styles.link + ' ' + styles.active} aria-current={isCurrentLink(link.href, pathname) ? 'page' : 'false'}>
-            {link.label}
-          </Link>
-        ))}
-      </nav>
-    </header>
-  )
+export const Header = async () => {
+  const data = await getVersion()
+  const latestVersion = data['dist-tags'].latest
+
+  return <Headers version={latestVersion} />
 }

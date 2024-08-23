ac6b50133b647c354b26cee98be6b4dabe6cd4f1
S.Watanabe <ocelot2828@gmail.com>
2024-08-23 16:10:19 +0900

    fix(components*): top page is server component

--- src/components/Firemotion/Animation.css.tsx
+++ src/components/Firemotion/Animation.css.tsx
@@ -1,3 +1,5 @@
+'use client'
+
 import { ReactNode } from 'react'
 import useFiremotion from 'firemotion'
 import Style from 'typedcssx'
--- src/components/Home/index.tsx
+++ src/components/Home/index.tsx
@@ -1,5 +1,3 @@
-'use client'
-
 import Link from 'next/link'
 import { styles } from './style.css'
 import Animation from 'components/Firemotion/Animation.css'

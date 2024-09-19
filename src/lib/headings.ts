00f46c4d82c6d6ff9897181de378023239026cf8
S.Watanabe <ocelot2828@gmail.com>
2024-09-19 12:22:37 +0900

    feat(headings.ts): add structures mobile menu

--- src/lib/headings.ts
+++ src/lib/headings.ts
@@ -1,11 +1,32 @@
 export const headings = [
   {
     heading: [
-      { id: 'zero-runtime-and-rsc', text: '# Zero Runtime and RSC' },
-      { id: 'hard-type-and-hot-reload', text: '# Hard Type and Hot Reload' },
-      { id: 'production-environment', text: '# Production environment' },
+      { id: 'zero-runtime', text: '# Zero Runtime' },
+      { id: 'type-safe', text: '# Type safe' },
+      { id: 'dry-principle', text: '# Dry principle' },
+      { id: 'how-to-use', text: '# How to use' },
       { id: 'inspirations', text: '# Inspirations' },
       { id: 'whats-next', text: "# What'sNext" }
+    ]
+  },
+  {
+    heading: [
+      { id: 'ease-of-introduction', text: '# Ease of introduction' },
+      { id: 'concise-api', text: '# Concise API' },
+      { id: 'simple-methods', text: '# Simple methods' },
+      { id: 'static-style', text: '# Static style' },
+      { id: 'avoiding-complexity', text: '# Avoiding complexity' },
+      { id: 'complete-static-determinism', text: '# Complete static determinism' },
+      { id: 'pre-compilation', text: '# Pre compilation' },
+      { id: 'ease-of-debugging', text: '# Ease of debugging' },
+      { id: 'performance', text: '# Performance' },
+      { id: 'optimization', text: '# Optimization' },
+      { id: 'scope-range', text: '# Scope range' },
+      { id: 'collision-lottery-range', text: '# Collision lottery range' },
+      { id: 'class-name-readablity', text: '# Class Name readability' },
+      { id: 'universally', text: '# Universally' },
+      { id: 'reason-for-object-notation', text: '# Reason for object notation' },
+      { id: 'shareble-objects', text: '# Shareable objects' }
     ]
   },
   {

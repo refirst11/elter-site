470645165f43089daac64ae35c49b1731ece58bf
S.Watanabe <ocelot2828@gmail.com>
2024-09-24 15:48:57 +0900

    fix(app/page.tsx): update meta title

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@ import { Home } from 'components/Home'
 import generateSEOData from 'lib/generateSEOData'
 import { Metadata } from 'next'
 
-export const metadata: Metadata = generateSEOData({ title: 'Home', subtitle: 'Typed CSS X - The Future of Styling with TypeScript and CSS' })
+export const metadata: Metadata = generateSEOData({ title: 'Documentation', subtitle: 'Typed CSS X - The Future of Styling with TypeScript and CSS' })
 
 export default function Page() {
   return <Home />

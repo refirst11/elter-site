73efe83d67bc46bb5f20b4aff456727102af2eec
S.Watanabe <ocelot2828@gmail.com>
2024-10-02 12:38:45 +0900

    feat(app/page.tsx): update description

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,10 @@ import { Home } from 'components/Home'
 import generateSEOData from 'lib/generateSEOData'
 import { Metadata } from 'next'
 
-export const metadata: Metadata = generateSEOData({ title: 'Documentation', subtitle: 'Typed CSS X - The Future of Styling with TypeScript and CSS' })
+export const metadata: Metadata = generateSEOData({
+  title: 'Documentation',
+  subtitle: 'TypedCSSX is a CSS-in-JS library for TypeScript that creating performance type-safe styles, high productivity and code quality.'
+})
 
 export default function Page() {
   return <Home />

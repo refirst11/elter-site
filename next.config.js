bf642e4474a7b44100ad6601d6efdcf9544a5ba6
S.Watanabe <ocelot2828@gmail.com>
2024-10-09 12:51:37 +0900

    feat(package): update typedcssx and next-plugin

--- next.config.js
+++ next.config.js
@@ -1,11 +1,10 @@
-const { TypedCSSXNextPlugin, configCSSModule } = require('typedcssx-next-plugin')
+const { configCSSModule } = require('typedcssx-next-plugin')
 
 /** @type {import('next').NextConfig} */
 
 const nextConfig = {
   webpack: (config) => {
     config.experiments = { ...config.experiments, topLevelAwait: true }
-    config.plugins.push(new TypedCSSXNextPlugin())
     return configCSSModule(config)
   },
   transpilePackages: ['next-mdx-remote']

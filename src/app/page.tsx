7252fca9f3f6bfb6abfc9d40e139d311118660cf
Refirst <75829584+refirst11@users.noreply.github.com>
2024-10-16 08:55:44 +0900

    chore(app/page.tsx): add meta description

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import generateSEOData from 'lib/generateSEOData'
 import { Metadata } from 'next'
 
 export const metadata: Metadata = generateSEOData({
-  title: 'Documentation',
+  title: 'Documentation | TypedCSSX',
   subtitle: 'TypedCSSX is a CSS-in-JS library for TypeScript that creating performance type-safe styles, high productivity and code quality.'
 })
 

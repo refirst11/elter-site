552652dd0d0f05263b09c16cd394499585498c1e
S.Watanabe <ocelot2828@gmail.com>
2024-11-03 11:00:58 +0900

    feat(sitemap.ts): dynamic sitemap

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -1,8 +1,13 @@
 import type { MetadataRoute } from 'next'
+import getAllPosts from 'lib/getAllPosts'
 
-export default function sitemap(): MetadataRoute.Sitemap {
+export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
   const url = process.env.PROD_URL || ''
   const currentDate = new Date()
+  const docs = await getAllPosts('documentation')
+  const api = await getAllPosts('api')
+  const inher = await getAllPosts('inheritance')
+  const anima = await getAllPosts('animation')
 
   const sitemapEntries: MetadataRoute.Sitemap = [
     {
@@ -10,74 +15,32 @@ export default function sitemap(): MetadataRoute.Sitemap {
       lastModified: currentDate,
       changeFrequency: 'yearly',
       priority: 1
-    },
-    {
-      url: `${url}/overview`,
-      lastModified: currentDate,
-      changeFrequency: 'monthly',
-      priority: 0.9
-    },
-    {
-      url: `${url}/installation`,
-      lastModified: currentDate,
-      changeFrequency: 'monthly',
-      priority: 0.8
-    },
-    {
-      url: `${url}/core-api/style/create`,
-      lastModified: currentDate,
-      changeFrequency: 'monthly',
-      priority: 0.8
-    },
-    {
-      url: `${url}/core-api/style/set`,
-      lastModified: currentDate,
-      changeFrequency: 'monthly',
-      priority: 0.8
-    },
-    {
-      url: `${url}/core-api/style/global`,
-      lastModified: currentDate,
-      changeFrequency: 'monthly',
-      priority: 0.8
-    },
-    {
-      url: `${url}/core-api/style/root`,
-      lastModified: currentDate,
-      changeFrequency: 'monthly',
-      priority: 0.8
-    },
-    {
-      url: `${url}/helpers/breakpoints`,
-      lastModified: currentDate,
-      changeFrequency: 'monthly',
-      priority: 0.8
-    },
-    {
-      url: `${url}/helpers/keyframes`,
-      lastModified: currentDate,
-      changeFrequency: 'monthly',
-      priority: 0.8
-    },
-    {
-      url: `${url}/helpers/selector`,
-      lastModified: currentDate,
-      changeFrequency: 'monthly',
-      priority: 0.8
-    },
-    {
-      url: `${url}/helpers/theme`,
-      lastModified: currentDate,
-      changeFrequency: 'monthly',
-      priority: 0.8
-    },
-    {
-      url: `${url}/hooks/useFiremotion`,
-      lastModified: currentDate,
-      changeFrequency: 'monthly',
-      priority: 0.8
     }
   ]
 
-  return sitemapEntries
+  const documentation: MetadataRoute.Sitemap = docs.map((docs) => ({
+    url: `${url}/${docs.slug}`,
+    lastModified: currentDate,
+    priority: 1
+  }))
+
+  const coreapi: MetadataRoute.Sitemap = api.map((api) => ({
+    url: `${url}/core-api/${api.slug}`,
+    lastModified: currentDate,
+    priority: 1
+  }))
+
+  const inheritance: MetadataRoute.Sitemap = inher.map((inher) => ({
+    url: `${url}/inheritance/${inher.slug}`,
+    lastModified: currentDate,
+    priority: 1
+  }))
+
+  const animation: MetadataRoute.Sitemap = anima.map((anima) => ({
+    url: `${url}/animation/${anima.slug}`,
+    lastModified: currentDate,
+    priority: 1
+  }))
+
+  return [...sitemapEntries, ...documentation, ...coreapi, ...inheritance, ...animation]
 }
